f7ee423056d93f2391897d67659fa6e43388db5e
mo <mo@edraj.io>
2018-01-26 02:06:42 +0200

    fixed tests

--- src/storage_test.go
+++ src/storage_test.go
@@ -10,28 +10,28 @@ import (
 )
 
 var (
-	storage   = Storage{RootPath: "/tmp/edraj/content", TrashPath: "/tmp/edraj/trash"}
+	storage = Storage{RootPath: "/tmp/edraj/content", TrashPath: "/tmp/edraj/trash"}
 
-	dirMeta     = DirMeta {
-		ID: "1",
-		OwnerID: "1000",
+	dirMeta = DirMeta{
+		ID:          "1",
+		OwnerID:     "1000",
 		Permissions: []string{"read", "write", "execute"},
-		Tags: []string{"home", "test"},
-		Categories: []string{"Private", "Family"},
+		Tags:        []string{"home", "test"},
+		Categories:  []string{"Private", "Family"},
 	}
 
-	fileMeta    = FileMeta {
-		ID: "2",
-		OwnerID: "1000",
+	fileMeta = FileMeta{
+		ID:          "2",
+		OwnerID:     "1000",
 		Permissions: []string{"read", "write", "execute"},
-		Tags: []string{"home", "test"},
-		Categories: []string{"Private", "Family"},
+		Tags:        []string{"home", "test"},
+		Categories:  []string{"Private", "Family"},
 		ContentType: "Text",
-		AuthorID: "1",
-		Signature: "signiture", // Replace with appropriate test value
-		Payload: "Hello World",
-		Checksum: "93623ac7d9badb95b01f74ceb2d17702f142e692",
-		Schema: "MySchema",
+		AuthorID:    "1",
+		Signature:   "signiture", // Replace with appropriate test value
+		Payload:     "Hello World",
+		Checksum:    "93623ac7d9badb95b01f74ceb2d17702f142e692",
+		Schema:      "MySchema",
 	}
 )
 
@@ -56,16 +56,18 @@ func TestCanonicalPath(t *testing.T) {
 
 func TestValidDir(t *testing.T) {
 	fmt.Printf("Testing method ValidDir...\n")
-	storage.ValidDir("/content/Dir", true)
+	storage.ValidDir("/Dir", true)
 
-  exists("/tmp/edraj/content/Dir") // Json verification will be added soon
-
+	state, _ := exists("/tmp/edraj/content/Dir/") // Json verification will be added soon
+	if state == false {
+		log.Fatal("Doesn't Exist!")
+	}
 
 }
 
 func TestPutDirMeta(t *testing.T) {
 	fmt.Printf("Testing method PutDirMeta...\n")
-	storage.PutDirMeta("/content/Dir", dirMeta)
+	storage.PutDirMeta("/Dir", dirMeta)
 
 	state, _ := exists("/tmp/edraj/content/Dir/.meta.json") // Json verification will be added soon
 	if state == false {
@@ -77,7 +79,7 @@ func TestPutDirMeta(t *testing.T) {
 // The function creates the metafile even when the target file isnt in the path given
 func TestPutFileMeta(t *testing.T) {
 	fmt.Printf("Testing method PutFileMeta...\n")
-	storage.PutFileMeta("/content/Dir/test.todo", fileMeta)
+	storage.PutFileMeta("/Dir/test.todo", fileMeta)
 
 	state, _ := exists("/tmp/edraj/content/Dir/.test.todo.meta.json") // Json verification will be added soon
 	if state == false {
@@ -88,7 +90,7 @@ func TestPutFileMeta(t *testing.T) {
 
 func TestGetFileMeta(t *testing.T) {
 	fmt.Printf("Testing method GetFileMeta...\n")
-	object, _ := storage.GetFileMeta("/content/Dir/test.todo")
+	object, _ := storage.GetFileMeta("/Dir/test.todo")
 
 	if object.ID != fileMeta.ID || object.OwnerID != fileMeta.OwnerID || object.ContentType != fileMeta.ContentType || object.AuthorID != fileMeta.AuthorID || object.Signature != fileMeta.Signature || object.Payload != fileMeta.Payload || object.Checksum != fileMeta.Checksum || object.Schema != fileMeta.Schema {
 		log.Fatal("GetFileMeta returned corrupt fileMeta")
@@ -100,7 +102,7 @@ func TestGetFileMeta(t *testing.T) {
 
 func TestGetDirMeta(t *testing.T) {
 	fmt.Printf("Testing method GetDirMeta...\n")
-	object, _ := storage.GetDirMeta("/content/Dir")
+	object, _ := storage.GetDirMeta("/Dir")
 
 	state, _ := exists("/tmp/edraj/content/Dir")
 	if state == false {
@@ -119,7 +121,7 @@ func TestGetDirMeta(t *testing.T) {
 func TestListDir(t *testing.T) {
 	fmt.Printf("Testing method ListDir...\n")
 
-	list, _ := storage.ListDir("/content/Dir") //
+	list, _ := storage.ListDir("/Dir") //
 	//syslist := exec.Command("find", "/tmp/edraj/content/Dir")
 	// Under Construction
 	// for x := 0; x > 10; x++ {
@@ -131,21 +133,21 @@ func TestListDir(t *testing.T) {
 func TestDeleteFile(t *testing.T) {
 	fmt.Printf("Testing method DeletFile...\n")
 
-	exec.Command("sh", "-c", "touch /tmp/edraj/content/Dir/TEST").Output()
-	time.Sleep(1 * time.Millisecond)
+	exec.Command("sh", "-c", "touch /tmp/edraj/content/Dir/DelFileTEST").Output()
+	time.Sleep(100 * time.Millisecond)
 
-	state, _ := exists("/tmp/edraj/content/Dir/TEST")
+	state, _ := exists("/tmp/edraj/content/Dir/DelFileTEST")
 	if state == false {
 		log.Fatal("Doesn't Exist!")
 	}
 
-	storage.DeleteFile("/content/Dir/TEST")
-	state, _ = exists("/tmp/edraj/content/Dir/TEST")
+	storage.DeleteFile("/Dir/DelFileTEST")
+	state, _ = exists("/tmp/edraj/content/Dir/DelFileTEST")
 	if state == true {
 		log.Fatal("File Still Exists!")
 	}
 
-	state, _ = exists("/tmp/edraj/trash/content/Dir/TEST")
+	state, _ = exists("/tmp/edraj/trash/Dir/DelFileTEST")
 	if state == false {
 		log.Fatal("File Doesn't Exists!")
 	}
@@ -155,16 +157,16 @@ func TestDeleteFile(t *testing.T) {
 func TestDeleteDir(t *testing.T) {
 	fmt.Printf("Testing method DeletDir...\n")
 
-	exec.Command("sh", "-c", "mkdir  /tmp/edraj/content/Dir/TEST").Output()
+	exec.Command("sh", "-c", "mkdir  /tmp/edraj/content/Dir/DelDirTEST").Output()
 	time.Sleep(100 * time.Millisecond)
 
-	state, _ := exists("/tmp/edraj/content/Dir/TEST")
+	state, _ := exists("/tmp/edraj/content/Dir/DelDirTEST")
 	if state == false {
 		log.Fatal("Doesn't Exist!")
 	}
 
-	storage.DeleteDir("/content/Dir/TEST/")
-	state, _ = exists("/tmp/edraj/content/Dir/TEST")
+	storage.DeleteDir("/Dir/DelDirTEST/")
+	state, _ = exists("/tmp/edraj/content/Dir/DelDirTEST")
 	if state == true {
 		log.Fatal("Dir Still Exists!")
 	}
@@ -173,44 +175,41 @@ func TestDeleteDir(t *testing.T) {
 func TestMoveFile(t *testing.T) {
 	fmt.Printf("Testing method MoveFile...\n")
 
-	exec.Command("sh", "-c", "touch /tmp/edraj/content/Dir/TEST").Output()
+	exec.Command("sh", "-c", "touch /tmp/edraj/content/Dir/MvFileTEST").Output()
 	time.Sleep(100 * time.Millisecond)
-	state, _ := exists("/tmp/edraj/content/Dir/TEST")
+	state, _ := exists("/tmp/edraj/content/Dir/MvFileTEST")
 	if state == false {
 		log.Fatal("Doesn't Exist!")
 	}
 
-	storage.MoveFile("/content/Dir/TEST", "/content/TEST")
-	state, _ = exists("/tmp/edraj/content/Dir/TEST")
+	storage.MoveFile("/Dir/MvFileTEST", "/MvFileTEST")
+	state, _ = exists("/tmp/edraj/content/Dir/MvFileTEST")
 	if state == true {
 		log.Fatal("File Still Exists!")
 	}
-	state, _ = exists("/tmp/edraj/content/TEST")
+	state, _ = exists("/tmp/edraj/content/MvFileTEST")
 	if state == false {
 		log.Fatal("File Doesn't Exists!")
 	}
-	exec.Command("sh", "-c", "rm /tmp/edraj/content/TEST").Output()
-	time.Sleep(100 * time.Millisecond)
-
 }
 
 // Moving Dirs Doesnt work for some reason
 func TestMoveDir(t *testing.T) {
 	fmt.Printf("Testing method MoveDir...\n")
 
-	exec.Command("sh", "-c", "mkdir /tmp/edraj/content/Dir/TEST").Output()
+	exec.Command("sh", "-c", "mkdir /tmp/edraj/content/Dir/MvDirTEST").Output()
 	time.Sleep(100 * time.Millisecond)
-	state, _ := exists("/tmp/edraj/content/Dir/TEST")
+	state, _ := exists("/tmp/edraj/content/Dir/MvDirTEST")
 	if state == false {
 		log.Fatal("Doesn't Exist!")
 	}
 
-	storage.MoveDir("/content/Dir/TEST", "/content/TEST")
-	state, _ = exists("/tmp/edraj/content/Dir/TEST")
+	storage.MoveDir("/Dir/MvDirTEST", "/MvDirTEST")
+	state, _ = exists("/tmp/edraj/content/Dir/MvDirTEST")
 	if state == true {
 		log.Fatal("Dir Still Exists!")
 	}
-	state, _ = exists("/tmp/edraj/content/TEST")
+	state, _ = exists("/tmp/edraj/content/MvDirTEST")
 	if state == false {
 		log.Fatal("Dir Doesn't Exists!")
 	}
