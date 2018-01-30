f248c16eb012a08c8bc4c6b4a17e46c44dd65acc
Kefah Issa <kefah.issa@gmail.com>
2018-01-30 08:18:11 -0800

    update

--- src/api.go
+++ src/api.go
@@ -232,7 +232,6 @@ func (server *Server) Hello(iIn Identity, sIn Signature, messageIn string) (iOut
 	// ? Is there a need to establish some form of a basic session here, after verification of the signature?
 	return Identity{}, Signature{}, "Hello back how can I help you?",
 		"token of verification - alternative to session"
-
 }
 
 /*
--- src/pki.go
+++ src/pki.go
@@ -27,34 +27,32 @@ import (
 
 func pkiExamples() {
 
-	keypair1 := RsaKeypair{}
+	keypair := RsaKeypair{}
 	var err error
-	err = keypair1.generate()
+	err = keypair.Generate()
 	if err != nil {
 		log.Fatal("While generating", err.Error())
 	}
 
-	private, err := keypair1.privatePEM()
+	private, err := keypair.PrivatePEM()
 	if err != nil {
 		log.Fatal("asString:", "private:", private, "/err:", err.Error())
 	}
-	public, err := keypair1.publicPEM()
+	public, err := keypair.PublicPEM()
 	if err != nil {
 		log.Fatal("asString:", "public:", public, "/err:", err.Error())
 	}
 
 	fmt.Println("public: \n"+public, "private: \n"+private)
-	keypair := RsaKeypair{}
+	publicOnlyKeypair := RsaKeypair{}
 	//err = keypair.loadPrivatePEM([]byte(private))
-	err = keypair.loadPublicPem([]byte(public))
+	err = publicOnlyKeypair.LoadPublicPem([]byte(public))
 	if err != nil {
 		log.Fatal("Load:", err.Error())
 	}
 
-	fmt.Println(keypair)
-
 	data := "Hello World"
-	signature, err := keypair1.sign([]byte(data))
+	signature, err := keypair.Sign([]byte(data))
 	if err != nil {
 		log.Fatal("Sign:", err.Error())
 	}
@@ -62,18 +60,17 @@ func pkiExamples() {
 	sigb64 := base64.StdEncoding.EncodeToString(signature)
 	fmt.Printf("Signature: %v\n", sigb64)
 
-	err = keypair.verify([]byte(data), signature)
+	err = publicOnlyKeypair.Verify([]byte(data), signature)
 	if err != nil {
 		log.Fatal("Verification failed:", err.Error())
 	}
 
-	enc, err := keypair.encrypt([]byte("Hello there"), []byte("some label"))
+	enc, err := publicOnlyKeypair.Encrypt([]byte("Hello there"), []byte("some label"))
 	if err != nil {
 		log.Fatalf("encrypt: %s", err)
 	}
 
-	// Decrypt the data
-	dec, err := keypair1.decrypt(enc, []byte("some label"))
+	dec, err := keypair.Decrypt(enc, []byte("some label"))
 	if err != nil {
 		log.Fatalf("decrypt: %s", err)
 	}
@@ -81,13 +78,14 @@ func pkiExamples() {
 	fmt.Println("All good")
 }
 
-// RsaKeypair the pub/private
+// RsaKeypair PKI
 type RsaKeypair struct {
-	public  *rsa.PublicKey // This is essentially Private.PublicKey
+	public  *rsa.PublicKey
 	private *rsa.PrivateKey
 }
 
-func (keypair *RsaKeypair) generate() (err error) {
+// Generate new keypair
+func (keypair *RsaKeypair) Generate() (err error) {
 	keypair.private, err = rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
 		return
@@ -96,15 +94,18 @@ func (keypair *RsaKeypair) generate() (err error) {
 	return
 }
 
-func (keypair *RsaKeypair) encrypt(clear []byte, label []byte) ([]byte, error) {
+// Encrypt data
+func (keypair *RsaKeypair) Encrypt(clear []byte, label []byte) ([]byte, error) {
 	return rsa.EncryptOAEP(sha256.New(), rand.Reader, keypair.public, clear, label)
 }
 
-func (keypair *RsaKeypair) decrypt(encrypted []byte, label []byte) ([]byte, error) {
+// Decrypt data
+func (keypair *RsaKeypair) Decrypt(encrypted []byte, label []byte) ([]byte, error) {
 	return rsa.DecryptOAEP(sha256.New(), rand.Reader, keypair.private, encrypted, label)
 }
 
-func (keypair *RsaKeypair) privatePEM() (string, error) {
+// PrivatePEM produce text-pem formated string for the private key
+func (keypair *RsaKeypair) PrivatePEM() (string, error) {
 	var err error
 	var buffer bytes.Buffer
 	key := &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(keypair.private)}
@@ -114,7 +115,9 @@ func (keypair *RsaKeypair) privatePEM() (string, error) {
 	}
 	return buffer.String(), nil
 }
-func (keypair *RsaKeypair) publicPEM() (string, error) {
+
+// PublicPEM produce text-pem formatted string for the public key
+func (keypair *RsaKeypair) PublicPEM() (string, error) {
 	var err error
 
 	data, err := x509.MarshalPKIXPublicKey(keypair.public)
@@ -132,15 +135,16 @@ func (keypair *RsaKeypair) publicPEM() (string, error) {
 	return buffer.String(), nil
 }
 
-func (keypair *RsaKeypair) sign(data []byte) ([]byte, error) {
+// Sign data
+func (keypair *RsaKeypair) Sign(data []byte) ([]byte, error) {
 	hash := sha256.New()
 	hash.Write(data)
 	dataHash := hash.Sum(nil)
-	signature, err := rsa.SignPKCS1v15(rand.Reader, keypair.private, crypto.SHA256, dataHash)
-	return signature, err
+	return rsa.SignPKCS1v15(rand.Reader, keypair.private, crypto.SHA256, dataHash)
 }
 
-func (keypair *RsaKeypair) verify(data []byte, signature []byte) error {
+// Verify previously signed data
+func (keypair *RsaKeypair) Verify(data []byte, signature []byte) error {
 	hash := sha256.New()
 	hash.Write(data)
 	dataHash := hash.Sum(nil)
@@ -148,9 +152,10 @@ func (keypair *RsaKeypair) verify(data []byte, signature []byte) error {
 	return err
 }
 
-func (keypair *RsaKeypair) loadPublicPem(publicPEM []byte) error {
-	// From now this is public-key only
-	// Hence only encrypt and verify can be used
+// LoadPublicPem loads publickey from a pem string.
+// Note: this disables all the private-key based functions
+// Only Encrypt and Verify are possible
+func (keypair *RsaKeypair) LoadPublicPem(publicPEM []byte) error {
 	keypair.private = nil
 	var block *pem.Block
 	var err error
@@ -181,7 +186,8 @@ func (keypair *RsaKeypair) loadPublicPem(publicPEM []byte) error {
 	return nil
 }
 
-func (keypair *RsaKeypair) loadPrivatePEM(privatePEM []byte /*, public []byte*/) error {
+// LoadPrivatePEM Parse and load text-pem formatted private key string
+func (keypair *RsaKeypair) LoadPrivatePEM(privatePEM []byte) error {
 	var block *pem.Block
 	var err error
 
